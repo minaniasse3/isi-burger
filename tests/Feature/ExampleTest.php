9f25150c8a4742ad6b39398119cfc83b4d6228f6
ndeye yacine niass <158225679+minaniasse3@users.noreply.github.com>
2025-03-19 18:03:55 +0000

    ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -10,12 +9,10 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-   public function test_application_returns_successful_response()
-{
-    $response = $this->get('/catalog'); // Adjust the route if necessary
+    public function test_application_returns_successful_response()
+    {
+        $response = $this->get('/catalog'); // Ajustez le chemin si nécessaire
 
-    $response->assertStatus(200); // If 302 is expected, handle it accordingly
-} $response->assertStatus(200);
-}
-
+        $response->assertStatus(200); // Si un code 302 est attendu, gérez-le en conséquence
+    }
 }
